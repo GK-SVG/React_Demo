ff8026a2d86d2edc5a16999c013576d4cc581c80
gk-svg <gk32239@gmail.com>
2020-10-05 20:31:02 +0530

    class component

---
+++ src/Demo_Component.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default class Home extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>Home Component</h1>
+                <h3>Home component hello</h3>
+            </div>
+        )
+    }
+}
